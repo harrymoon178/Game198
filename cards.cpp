3595cf8f37619787c0b0467195a91f6ed819b5bf
jkjk101 <66657340+jkjk101@users.noreply.github.com>
2021-04-08 21:48:39 +0800

    Added cards file

---
+++ cards.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdlib>      // for calling srand(), rand()
+#include <ctime>        // for calling time()
+#include <vector>
+
+#define SPADE   "\xE2\x99\xA0"
+#define CLUB    "\xE2\x99\xA3"
+#define HEART   "\xE2\x99\xA5"
+#define DIAMOND "\xE2\x99\xA6"
+
+using namespace std;
+
+struct Card {
+  int idx;
+  int rank_idx;
+  int suit_idx;
+  string rank;
+  string suit;
+
+  Card(int card_idx) {
+    idx = card_idx;
+    rank_idx = card_idx % 13;  // "A","2","3","4","5","6","7","8","9","10","J","Q","K"
+    suit_idx = card_idx / 13;  // suit 1 = SPADE; 2 = HEART; 3 = CLUB; 4 = DIAMOND
+    string ranks[] = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    string suits[] = {SPADE, HEART, CLUB, DIAMOND};
+    rank = ranks[rank_idx];
+    suit = suits[suit_idx];
+  }
+};
+
+vector<Card> cards;
+
+bool sameCard(int card_idx)
+{
+  for (int i = 0; i < cards.size(); i++)
+    if (cards[i].idx == card_idx)
+      return 1;
+  return 0;
+}
+
+void randomCard()
+{
+  srand(time(NULL)); // seeding the pseudo-random generator
+  int card_idx = rand()%52;   // generate random card
+
+  while (sameCard(card_idx)) //loop until no same card is generated
+    card_idx = rand()%52;
+
+  Card c(card_idx);
+  cards.push_back(c);   // add card to cards
+}
+
+void printCards()
+{
+  for (int i = 0; i < cards.size(); i++) {
+    cout << cards[i].rank << cards[i].suit << ' ';
+  }
+  cout << endl;
+}
