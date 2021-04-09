292d40cc476f8a34088771451f6ee58c7b847238
jkjk101 <66657340+jkjk101@users.noreply.github.com>
2021-04-09 11:19:36 +0800

    Add files via upload

---
+++ menu.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include "user.h"
+#include "menu.h"
+
+using namespace std;
+
+vector<User> users;
+
+void selectMode(User usr) {
+  cout << "Choose a game mode: " << endl;
+  cout << "1 - Single Player" << endl;
+  cout << "2 - vsComputer" << endl;
+
+  int mode;
+  cin >> mode;
+  //if (mode == 1)
+    //single(usr);
+  //else if (mode == 2)
+    //vsComputer(usr);
+}
+
+bool sameName(string name) {
+  for (int i = 0; i < users.size(); i++)
+    if (name == users[i].name)
+      return 1;
+  return 0;
+}
+
+void newUser() {
+  string name;
+  cout << "New Player: ";
+  cin >> name;
+
+  while (sameName(name)) {
+    cout << "Name taken, try another: ";
+    cin >> name;
+  }
+  User u(name);
+  users.push_back(u);
+
+  selectMode(u);
+}
+
+void selectUser() {
+  if (users.size() == 0)
+    cout << "No player record." << endl;
+  else {
+    for (int i = 0; i < users.size(); i++) {
+      cout << i+1 << ". ";
+      users[i].print_info();
+    }
+    int u;
+    cin >> u;
+    selectMode(users[u-1]);
+  }
+}
+
+void printLeaderboard() {
+  if (users.size() == 0)
+    cout << "No player record." << endl;
+  else {
+    vector<User> ranking = users;
+    sort(ranking.begin(), ranking.end());
+
+    for (int i = 0; i < users.size(); i++)
+      cout << "No." << i+1 << " " << ranking[i].name << " " << ranking[i].score << endl;
+  }
+}
+
+void loadGame() {
+  string load_file;
+  cout << "Load from (file name): ";
+  cin >> load_file;
+
+  ifstream fin(load_file.c_str());
+
+  if (fin.fail())
+    cout << "File access error" << endl;
+  else {
+    users.clear();
+    string name;
+    while (fin >> name) {
+      int money, score, level;
+      fin >> money >> score >> level;
+      User u(name, money, score, level);
+      users.push_back(u);
+    }
+    fin.close();
+  }
+}
+
+void saveGame() {
+  string save_file;
+  cout << "Save as (file name): ";
+  cin >> save_file;
+
+  ofstream fout(save_file.c_str());
+
+  if (fout.fail())
+    cout << "File access error" << endl;
+  else {
+    for (int i = 0; i < users.size(); i++)
+      fout << users[i].name << ' ' << users[i].money << ' ' << users[i].score << ' ' << users[i].level << endl;
+    fout.close();
+  }
+}
---
+++ menu.h
@@ -0,0 +1,22 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "user.h"
+
+using namespace std;
+
+void selectMode(User usr);
+
+bool sameName(string name);
+
+void newUser();
+
+void selectUser();
+
+void printLeaderboard();
+
+void loadGame();
+
+void saveGame();
+
+#endif
