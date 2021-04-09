0d4434e0e28767b18251366132c04da4f9f85d68
jkjk101 <66657340+jkjk101@users.noreply.github.com>
2021-04-09 11:17:19 +0800

    Add files via upload

---
+++ user.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "user.h"
+
+using namespace std;
+
+User::User(string name, int score, int level, int money) {
+  this->name = name;
+  this->score = score;
+  this->level = level;
+  this->money = money;
+}
+
+void User::reset() {
+  score = 0;
+  level = 1;
+}
+
+void User::print_info() {
+  cout << name << ": " << score << ", lv." << level << ", $" << money << endl;
+}
+
+bool operator<(const User & a, const User & b) {
+  if (a.score == b.score)
+    return (a.name < b.name);
+  return (a.score > b.score);
+}
