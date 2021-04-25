728fa8959bbaec68fce03abb041d0cf2974807c4
jkjk101 <66657340+jkjk101@users.noreply.github.com>
2021-04-26 04:33:19 +0800

    Update card.cpp
    changed the print_card function

--- card.cpp
+++ card.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>      // for calling srand(), rand()
 #include <ctime>        // for calling time()
+#include <iomanip>      // for setw()
 #include "card.h"
 
 Card::Card(int card_idx) {
@@ -13,24 +14,15 @@ Card::Card(int card_idx) {
   suit = suits[suit_idx];
 }
 
-void Card::print_card() { //print the outline of a card
-for (int i = 1; i <= 6; i++)
-  {
-      for (int j = 1; j <= 10; j++)
-      {
-          if (j==10)
-              cout << "* ";
-          else if (i == 1 || i == 6 ||
-              j == 1)        
-              cout << "*";            
-          else if((i==2&&j==2)||(i==5&&j==9))
-              cout << rank;
-          else if ((i==2&&j==3)||(i==5&&j==8))
-              cout << suit;
-          else
-              cout << " ";
-      }
-      cout << endl;
-  }
- 
+void Card::print_card() { // print the outline of a card
+  for (int i = 0; i < 12; i++)
+    cout << '*';
+  cout << endl;
+  cout << '*' << right << setw(2) << rank << suit << setw(8) << '*' << endl;
+  for (int i = 0; i < 5; i++)
+    cout << '*' << setw(11) << '*' << endl;
+  cout << '*' << setw(9) << suit << ' ' << setw(2) << left << rank << '*' << endl;
+  for (int i = 0; i < 12; i++)
+    cout << '*';
+  cout << endl;
 }
