c303b6081e4e1077f39a2bef34b236d5cef1bd80
kirawhc <71225667+kirawhc@users.noreply.github.com>
2021-04-26 15:32:56 +0800

    Update user.cpp

--- user.cpp
+++ user.cpp
@@ -8,11 +8,17 @@ User::User(string name, int score, int level, int money) { // account constructo
   this->money = money;
 }
 
-void User::reset() { // reset user score and level
+// Function: to reset the score and level of the user
+// Input: No input
+// Output: score is reset to 0 and level is backed to 1
+void User::reset() { 
   score = 0;
   level = 1;
 }
 
+// Function: to print the information of the user
+// Input: No input
+// Output: the information (name, score, level and money) of the user
 void User::print_info() {
   cout << name << ": " << score << ", lv." << level << ", $" << money << endl;
 }
